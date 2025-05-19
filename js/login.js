e3c68ceeae6c0482b0f167f6c298dc639f31e9a2
Yan Carlos <yan.c.oliveira08@gmail.com>
2025-05-19 14:58:37 -0300

    Configuração login
    Esta parte esta responsavel por verificar o login do usuario e indicar um erro ou redireciona-lo a uma outra pagina, embora ainda não possua logins especificos, provavelmente adicionarei com json

--- js/login.js
+++ js/login.js
@@ -1,27 +1,15 @@
 function verificarLogin() {
 
-    // Obtém os valores dos campos de entrada
-    // e verifica se estão vazios
 
     const nomeFuncionario = document.getElementById("nome_funcionario").value;
     const senhaFuncionario = document.getElementById("senha_funcionario").value;
     
-    // Verifica se os campos estão vazios
 
-    if (senhaFuncionario == "") {
-        alert("Preencha todos os campos.");
-        document.getElementById("senha_funcionario").focus();
+    if (senhaFuncionario == "" || nomeFuncionario == "") {
+        mensagemErro.style.display = "block";
         return;
     }
-    // Verifica se o nome do funcionário está vazio
-    else if (nomeFuncionario  == "") {
-        alert("Preencha todos os campos.");
-        document.getElementById("nome_funcionario").focus();
-        return;
-    }
-    // Redireciona para a página 'home.html' se os campos não estiverem vazios
     else{
-        // Redireciona para a página 'home.html'
         window.location.href = "home.html";
     }
 }

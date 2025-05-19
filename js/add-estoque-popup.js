54b67f8e07e525fdf75802f39671b3d0198bb3f0
Micael Junco <micaeljuncoplay4@gmail.com>
2025-05-19 17:06:04 -0300

    funções renomeadas e remoções
    renomeei as funções para não conflitarem com outros popups e removi a parte de clicar fora do popup para remover

--- js/add-estoque-popup.js
+++ js/add-estoque-popup.js
@@ -1,33 +1,21 @@
 // Função para mostrar o popup
-function mostrarPopup() {
+function mostrarPopupAdicionar() {
     document.getElementById('popup-add').style.display = 'flex';
     document.getElementById('backdrop-popup').style.display = 'block';
 }
 
 // Função para fechar o popup
-function fechar() {
+function fecharAdicionar() {
     document.getElementById('popup-add').style.display = 'none';
     document.getElementById('backdrop-popup').style.display = 'none';
 }
 
-// Função para fechar o popup ao clicar no botão "Fechar" ou "Cancelar"
-function cancelar() {
-    fechar();
-}
-
 // Adicionando evento de clique no backdrop (fundo)
 window.onload = function () {
-    const backdrop = document.getElementById('backdrop-popup');
-    if (backdrop) {
-        backdrop.addEventListener("click", function () {
-            fechar(); // Fecha o popup quando o fundo (backdrop) for clicado
-        });
-    }
-
-    const fecharButton = document.getElementById('fechar-button');
+    const fecharButton = document.getElementById('cancelar');
     if (fecharButton) {
         fecharButton.addEventListener("click", function () {
-            fechar(); // Fecha o popup quando o botão "Fechar" for clicado
+            fecharAdicionar(); // Fecha o popup quando o botão "Fechar" for clicado
         });
     }
 }
